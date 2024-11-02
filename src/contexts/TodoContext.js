034a72cf44e3e17279e823ae0d2dab2ed593053d
TejaSaii <tejas7833@gmail.com>
2024-11-02 23:07:17 +0530

    completed

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,19 @@
+import {createContext, useContext} from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false
+        }
+    ],
+    addTodo: () => {},
+    updateTodo: () => {},
+    deleteTodo: () => {},
+    toggleComplete: () => {}
+});
+
+export const useTodo = () => useContext(TodoContext);
+
+export const TodoProvider = TodoContext.Provider;
